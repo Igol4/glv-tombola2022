4f16a3f30b37b3cbc7a74f0db06a9fa801a16475
Igol4 <94765726+Igol4@users.noreply.github.com>
2022-01-19 22:36:36 +0100

    Update dbh.php

--- Page/dbh.php
+++ Page/dbh.php
@@ -2,13 +2,8 @@
 $host = "localhost";
 $dbname = "id18183276_tombola";
 $username = "id18183276_tombola2";
-$password = "|oQzD3&S-{OX&SB-";
+$password = "******";
 
 $conn=mysqli_connect($host,$username,$password,$dbname);
-/*if (mysqli_connect_errno()) {
-    echo("Connection failed: " . mysqli_connect_error());
-}
-
-echo "Connected successfully";*/
 
 ?>
