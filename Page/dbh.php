833bd407bcd23ef40c65f569e7fd1ea80a498c52
Ivan Golubić <94765726+Igol4@users.noreply.github.com>
2022-01-26 16:30:18 +0100

    Add files via upload

---
+++ Page/dbh.php
@@ -0,0 +1,9 @@
+<?php
+
+$server = "localhost";
+$dbname = "dbphpsearch";
+$username = "root1";
+$password = "8TFG.Qg_Bsq3wev";
+
+$conn = mysqli_connect($server, $username, $password, $dbname);
+?>
