f6d69310083afac3c0d509cc96a76393f2220d65
Ivan Golubić <94765726+Igol4@users.noreply.github.com>
2022-01-26 16:29:59 +0100

    Delete Page directory

--- Page/dbh.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$dbname = "id18183276_tombola";
-$username = "id18183276_tombola2";
-$password = "******";
-
-$conn=mysqli_connect($host,$username,$password,$dbname);
-
-?>
